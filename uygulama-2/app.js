3338dcef801b7b4d50e4250728e38a6f57f30343
fuatguzel <fuatguzel754@gmail.com>
2021-08-31 15:11:07 +0300

    Complete uygulama-2

---
+++ uygulama-2/app.js
@@ -0,0 +1,4 @@
+fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((response) => response.json())
+    .then((json) => json.forEach((item) => console.log(item.email)))
+    .catch((err) => console.log(err))
